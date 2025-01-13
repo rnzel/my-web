9dd5dff6a4a6891fb6f4ebe5798d9e5f0e7d374b
rnzel <gobrisranzel@gmail.com>
2025-01-13 16:33:42 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+const errorMessage = document.getElementById('errorMessage');
+
+const validCredentials = {
+    username: "love",
+    password: "1425",
+};
+
+signInButton.addEventListener('click', () => {
+    const username = document.getElementById('username').value.toLowerCase(); 
+    const password = document.getElementById('password').value;
+
+    if (username === validCredentials.username.toLowerCase() && password === validCredentials.password) { 
+        container.classList.add("right-panel-active");
+        errorMessage.style.display = "none"; 
+    } else {
+        errorMessage.style.display = "block";
+    }
+});
